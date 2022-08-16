985022b1f7311c2cd1b0e2d324f1e730868d3337
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-08-15 22:07:29 -0300

    confirmar

---
+++ src/views/PerfilAskon.vue
@@ -0,0 +1,15 @@
+<template>
+  <AskonPerfil/>
+</template>
+
+<script>
+import AskonPerfil from '@/components/AskonPerfil.vue'
+export default {
+    components:{AskonPerfil},
+    
+}
+</script>
+
+<style>
+
+</style>
